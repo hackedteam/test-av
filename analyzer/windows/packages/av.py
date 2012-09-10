22ba9c159c1a71487d764d123fd249d760a129f7
olli <olli@hackingteam.it>
2012-09-10 09:53:21 +0200

    Fixing AV package. Added library WMI

--- analyzer/windows/packages/av.py
+++ analyzer/windows/packages/av.py
@@ -2,6 +2,7 @@
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 import os
+import wmi
 
 from lib.common.abstracts import Package
 from lib.api.process import Process
@@ -10,16 +11,15 @@ class Av(Package):
     """AntiVirus analysis package."""
     def __init__(self):
         self._agent_dir = "\%APPDATA\%\\.."
+        self.c = wmi.WMI()
+        self.process_watcher = self.c.Win32_Process.watch_for("creation")
     
     def list_dir(self):
         dirs = os.listdir(self._agent_dir)
         return dir
 
     def start(self, path):
-        
-        #before = self.list_dir()
-        #print before
-        
+                
         p = Process()
 
         if "arguments" in self.options:
@@ -27,13 +27,15 @@ class Av(Package):
         else:
             p.execute(path=path, suspended=True)
         
-        #after = self.list_dir()
-        #print after
         print("sleeping for demo windows")
         sleep(15)
         return p.pid
 
     def check(self):
+        proc = self.process_watcher()
+        if proc:
+            print proc.Caption
+        
         return True
 
     def finish(self):
