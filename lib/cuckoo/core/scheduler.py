9069013e5e6fc1420a21e2b2cff9dd7c1fba3ba5
olli <olli@hackingteam.it>
2012-09-14 18:10:07 +0200

    fixing av test case (task id)

--- lib/cuckoo/core/scheduler.py
+++ lib/cuckoo/core/scheduler.py
@@ -175,13 +175,13 @@ class AnalysisManager(Thread):
             guest.save_results(self.analysis.results_folder)
         except (CuckooMachineError, CuckooGuestError) as e:
             raise CuckooAnalysisError(e)
-        
+        """
         finally:
             # Stop machine
             mmanager.stop(vm.label)
             # Release the machine from lock
             mmanager.release(vm.label)
-        
+        """
         # Launch reports generation
         Reporter(self.analysis.results_folder).run(Processor(self.analysis.results_folder).run())
 
