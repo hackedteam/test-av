181c408942a8cb05a767b5d938c233c706bfaf76
olli <olli@hackingteam.it>
2012-09-14 15:55:00 +0200

    fixing task table constructor

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -73,9 +73,9 @@ class Task(Base):
 	status = Column(Integer)
 	detected = Column(Integer)
 
-    def __init__(self,
-                id,
-                md5):
+    def __init__(self, id, a_id, md5, file_path, timeout,
+                priority, custom, machine, package, options,
+                platform, lock, status, detected):
         self.id = id
         self.a_id = a_id
         self.md5 = md5
