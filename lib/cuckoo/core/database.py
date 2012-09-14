dc628346e07589789944de403235f50966f8f555
olli <olli@hackingteam.it>
2012-09-14 13:49:02 +0200

    modeling db with SQLAlchemy, rewriting methods

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -8,6 +8,7 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import Column, Integer, Text, DateTime
 
 from lib.cuckoo.common.constants import CUCKOO_ROOT
