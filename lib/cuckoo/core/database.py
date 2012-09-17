9aac0b0231a95fff734c5cdc4abbb5d82da5fc4b
olli <olli@hackingteam.it>
2012-09-17 10:45:07 +0200

    fixin db relationships

--- lib/cuckoo/core/database.py
+++ lib/cuckoo/core/database.py
@@ -328,7 +328,7 @@ class Database:
         try:
             analysis = s.query(Analysis).order_by(desc(Analysis.status),desc(Analysis.created_on)).all()
             a = analysis
-            print analysis
+            print analysis.exe
             a.file_path = analysis.exe.file_path
             return a
         except SQLAlchemyError as e:
